214a531d39a4f2ffc8230f7ed739d66ee5636ca8
Katie Moore <kemoore1431@gmail.com>
2018-02-09 20:03:47 +0000

    Logic to make multiple blocks fall and stack on top of one another

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,13 @@ $(() => {
   console.log('JS Loaded');
 
   //Variables
-  const $cells = $('li');
+  // const $cells = $('li');
   const cells = [].slice.call($('li'));
   const lastCell = cells.length-1;
   let timerId = null;
   let shape = null;
 
+  //OBJECT STUFF
   //Objects
   // class Square {
   //   constructor() {
@@ -18,22 +19,25 @@ $(() => {
   //     this.sq4 = this.sq2 + 10;
   //   }
   // }
-
-  const square = [0, 1, 10, 11];
-  const newsquare = [0, 1, 10, 11];
-  const shapes = [square, newsquare];
-
+  //
   // const square = new Square();
   // shape = square;
+  // timerId = setInterval(move, 1000);
 
-  shapes.forEach((el) => {
-    shape = el;
+  //ARRAY STUFF
+  let shapes= [[0,1,10,11],[0,1,10,11]];
+  console.log(shape);
+  gamePlay();
+
+  function gamePlay() {
+    console.log(shapes);
+    console.log('started loop');
+    shape = shapes[Math.floor(Math.random()*shapes.length)];
     cellChange();
-    timerId = setInterval(move,1000);
-  });
-
+    timerId = setInterval(move, 1000);
+  }
   //Functions
-
+  //OBJECT VERSIONS
   // function cellChange() {
   //   cells.forEach((cell, i) => {
   //     console.log(shape);
@@ -42,6 +46,22 @@ $(() => {
   //   });
   // }
 
+  // function move() {
+  //   shape.sq1 += 10;
+  //   shape.sq2 = shape.sq1 + 1;
+  //   shape.sq3 = shape.sq1 + 10;
+  //   shape.sq4 = shape.sq2 + 10;
+  //   if (Object.values(shape).some((sq) => sq > lastCell)) {
+  //     cells.forEach(cell => {
+  //       if ($(cell).hasClass('red')) $(cell).addClass('fixed');
+  //     });
+  //     clearInterval(timerId);
+  //   } else {
+  //     cellChange();
+  //   }
+  // }
+
+  //ARRAY VERSIONS
   function cellChange() {
     cells.forEach((cell, i) => {
       if (shape.includes(i)) $(cell).addClass('red');
@@ -52,13 +72,15 @@ $(() => {
   function move() {
     shape[0] += 10;
     shape[1] = shape[0] + 1;
-    shape[2] = shape[0] + 10;
-    shape[3] = shape[1] + 10;
-    if (shape.some((sq) => sq > lastCell)) {
+    shape[2] = shape[2] + 10;
+    shape[3] = shape[3] + 10;
+    if (shape.some((sq) => sq > lastCell || shape.some((sq) => $(cells[sq]).hasClass('fixed')))) {
       cells.forEach(cell => {
         if ($(cell).hasClass('red')) $(cell).addClass('fixed');
       });
       clearInterval(timerId);
+      shapes = [[0,1,10,11],[0,1,10,11]];
+      gamePlay();
     } else {
       cellChange();
     }
