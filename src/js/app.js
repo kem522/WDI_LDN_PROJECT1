107fff976c019ab472b4d76e9b361a054cb2aaa8
Katie Moore <kemoore1431@gmail.com>
2018-02-09 15:28:20 +0000

    Created gameboard and logic for a single, falling square

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,42 @@
-[10,20,30].forEach(num => console.log(num));
+
+$(() => {
+  console.log('JS Loaded');
+
+  //Variables
+  const $cells = $('li');
+  const cells = [].slice.call($('li'));
+  const key = document.addEventListener('keydown', (e) => e.which);
+
+  const lastCell = cells.length-1;
+  console.log(lastCell);
+
+  //Objects
+  class Square {
+    constructor() {
+      this.sq1 = 0;
+      this.sq2 = this.sq1 + 1;
+      this.sq3 = this.sq1 + 10;
+      this.sq4 = this.sq2 + 10;
+    }
+  }
+
+  const square = new Square();
+  cellChange();
+
+  function cellChange() {
+    cells.forEach((cell, i) => {
+      if (Object.values(square).includes(i)) $(cell).addClass('red');
+      else $(cell).removeClass('red');
+    });
+  }
+
+  const timerId = setInterval(() => {
+    square.sq1 += key;
+    square.sq2 = square.sq1 + 1;
+    square.sq3 = square.sq1 + 10;
+    square.sq4 = square.sq2 + 10;
+    if (Object.values(square).some((sq) => sq > lastCell)) clearInterval(timerId);
+    else cellChange();
+  },1000);
+
+});
