e8b36b8f586be3bab2f7de95246189ee33adfa6b
Katie Moore <kemoore1431@gmail.com>
2018-02-10 15:48:26 +0000

    Rows will disappear when filled, imperfect

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-
 $(() => {
   console.log('JS Loaded');
 
@@ -20,6 +19,25 @@ $(() => {
   const colors = ['red','blue','yellow','green'];
   gamePlay();
 
+  const rowsArray = [
+    [0,1,2,3,4,5,6,7,8,9],
+    [11,12,13,14,15,16,17,18,19],
+    [21,22,23,24,25,26,27,28,29],
+    [31,32,33,34,35,36,37,38,39],
+    [41,42,43,44,45,46,47,48,49],
+    [51,52,53,54,55,56,57,58,59],
+    [61,62,63,64,65,66,67,68,69],
+    [71,72723,74,75,76,77,78,79],
+    [81,82,83,84,85,86,87,88,89],
+    [91,92,93,94,95,96,97,98,99],
+    [101,102,103,104,105,106,107,108,109],
+    [111,112,113,114,115,116,117,118,119],
+    [121,122,123,124,125,126,127,128,129],
+    [131,132,133,134,135,136,137,138,139],
+    [141,142,143,144,145,146,147,148,149],
+    [151,152,153,154,155,156,157,158,159]
+  ];
+
   //Functions
   function checkLoss() {
     for (let i = 0; i < 10; i++) {
@@ -31,27 +49,24 @@ $(() => {
     }
   }
 
-  // function checkRow() {
-  //   for (let i = 0; i < rows*columns; i += 10) {
-  //     let clear = true;
-  //     for (let x = i; x < i + columns; i++) {
-  //       if (!$(cells[i]).hasClass('fixed')) clear = false;
-  //     }
-  //     if (clear) {
-  //       for (let x = i; x < i + columns; i++) {
-  //         $(cells[i]).remove();
-  //       }
-  //     }
-  //   }
-  // }
+  function checkRow() {
+    for (let i = 0; i < rowsArray.length; i++) {
+      let clear = true;
+      rowsArray[i].forEach((el) => {
+        if (!$(cells[el]).hasClass('fixed')) {
+          clear = false;
+        }
+      });
+      if (clear) rowsArray[i].forEach((el) => $(cells[el]).remove());
+    }
+  }
 
   function gamePlay() {
     setTimeout(checkLoss, 2000);
-    // setTimeout(checkRow, 2000);
     color = colors[Math.floor(Math.random()*colors.length)];
     shape = shapes[Math.floor(Math.random()*shapes.length)];
     cellChange();
-    timerId = setInterval(move, 1000);
+    timerId = setInterval(move, 100);
   }
 
   function cellChange() {
@@ -71,6 +86,7 @@ $(() => {
     shape[2] = shape[2] + 10;
     shape[3] = shape[3] + 10;
     checkForEnd();
+    checkRow();
   }
 
   function horizontalMove() {
@@ -100,14 +116,6 @@ $(() => {
     }
     key = '';
   }
-
-  // function checkRow() {
-  //   let clear = true;
-  //   for (let i = cells.length - 10; i < cells.length; i++) {
-  //     if (!$(cells[i]).hasClass('fixed')) clear = false;
-  //   }
-  //   }
-  // }
 
   function checkForEnd() {
     if (shape.some((sq) => sq > lastCell || shape.some((sq) => $(cells[sq]).hasClass('fixed')))) {
